261b4ebbe70500288168cbf8b88f6fdb615bfbb0
haidang332002 <106529008+haidang332002@users.noreply.github.com>
2022-05-30 18:33:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,19 +2,19 @@ const CONFIG = {
     titleWeb: "Thế giới yêu thương",
     introTitle: 'Người bí ẩn ',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
+    Mà sao chị cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    btnIntro: 'Chị muốn nghe không.',
+    title: 'Hẹn chị năm anh 25 tuổi,khi đó anh sẽ theo đuổi chị với tư cách là người có thể lo cho chị 🥰',
+    desc: 'Chị đồng ý với anh không ... ',
+    btnYes: 'Đồng ý <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Hãy luôn vui vẻ và mỉm cười nha cô gái.',
-    btnAccept: 'Yêu em <',
+    question: 'Nói cho anh nghe lý do được không , bí mật của riêng , anh không không nói ai đâu <3',
+    btnReply: 'Gửi Anh <3',
+    reply: 'Chị biết tuổi tác là 1 phần nhưng cảm ơn anh đã yêu thương chị suốt thời gian qua , chị không dám nói trước nhưng chị sẽ cố gắng ở bên cạnh anh ngay cả những lúc anh khó khăn nhất',
+    mess: 'Anh biết mà 🥰. Yêu chị nhiều nhiều 😘😘',
+    messDesc: 'Hãy luôn vui vẻ và mỉm cười nha cô gái của Anh.',
+    btnAccept: 'Yêu em <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
