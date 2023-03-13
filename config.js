c665fd59025ad7b3768d05a76056f35c979c5925
Hải Đang <106529008+haidang332002@users.noreply.github.com>
2023-03-13 20:35:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
     titleWeb: "Thế giới yêu thương",
-    introTitle: 'Anh yêu của em! ',
+    introTitle: 'Boss của em! ',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'Em muốn nghe không.',
-    title: 'Anh có một trò chơi này , cuối tuần mặc bra nhá^^🥰',
+    title: 'Anh có một trò chơi này ,Cười nhiều lên , bớt nóng tính với lạnh lùng nhaaaaa',
     desc: 'Em đồng ý với anh không ... ',
     btnYes: 'Đồng ý <33',
     btnNo: 'Không nha :3',
-    question: 'hehe đồng ý rùi nghen , không chối đâu đó ,chụt chụt kkk , còn giờ nói anh nghe muốn mặc không nha ',
+    question: 'hehe đồng ý rùi nghen , không chối đâu đó ,chụt chụt kkk ',
     btnReply: 'Gửi Anh <3',
-    reply: 'Thật ra thì cũng đơn giản thui anh à , chỉ cần anh thích là được mà vì em là của anh r <3',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Hãy luôn vui vẻ và mỉm cười nha  chúa  của Anh..',
+    reply: 'Thật ra thì cũng đơn giản thui anh à , chỉ cần anh thích là được, người con trai đã gây cho em bao cảm xúc đặt biệt khó tả , cảm ơn anh , yêu anh , chàng trai của em',
+    mess: 'Anh biết mà 🥰. Yêu Cua nhiều nhiều 😘😘',
+    messDesc: 'Hãy luôn vui vẻ và mỉm cười nha Cô gái nhỏ của Anh..',
     btnAccept: 'Yêu em <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
